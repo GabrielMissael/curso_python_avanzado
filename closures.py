568da8b1a4027a5130d98aa23732bb501e442547
GabrielMissael <missael_gabo@hotmail.com>
2021-09-30 11:13:40 -0500

    Added closures example

---
+++ closures.py
@@ -0,0 +1,15 @@
+def make_repeater_of(n: int):
+    def repeater(string: str) -> str:
+        assert type(string) == str, "Solo puedes utilizar cadenas de texto 😄"
+        return n*string
+    return repeater
+
+def main():
+    repeat_5 = make_repeater_of(5)
+    print(repeat_5('Hey '))
+
+    repeat_10 = make_repeater_of(10)
+    print(repeat_10('Platzi '))
+
+if __name__=='__main__':
+    main()
